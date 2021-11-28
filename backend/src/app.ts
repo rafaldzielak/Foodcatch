b97317d4873a4303567f17ade43321142bbca5ce
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-28 11:56:22 +0100

    BE: Connect to mogno cluster

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { bookingSchema } from "./schema/rootSchema";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI!, () => console.log("Mongo connected"));
 
 app.use(cors());
 
