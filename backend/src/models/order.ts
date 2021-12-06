4aebe905fed2b98fa34a5f91409330b4d21d3859
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-12-06 08:47:22 +0100

    BE: Order model for DB

---
+++ backend/src/models/order.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+import { DishAttrs } from "./Dish";
+
+interface DishWithNumber extends DishAttrs {
+  amount: number;
+}
+
+export interface OrderAttrs {
+  name: string;
+  date: Date;
+  phone: string;
+  dish: DishWithNumber;
+  firstName: string;
+  surname: string;
+  street: string;
+  streetNumber: string;
+  city: string;
+  paymentMethod: "cash" | "card";
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  name: string;
+  date: Date;
+  phone: string;
+  dish: DishWithNumber;
+  firstName: string;
+  surname: string;
+  street: string;
+  streetNumber: string;
+  city: string;
+  paymentMethod: "cash" | "card";
+}
+
+const orderSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  phone: { type: String, required: true },
+  dish: { type: Schema.Types.Mixed },
+  firstName: { type: String, required: true },
+  surname: { type: String, required: true },
+  street: { type: String, required: true },
+  streetNumber: { type: String, required: true },
+  city: { type: String, required: true },
+  paymentMethod: { type: String, required: true },
+});
+
+orderSchema.statics.build = async (attrs: OrderAttrs) => {
+  return new Order({ ...attrs });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
