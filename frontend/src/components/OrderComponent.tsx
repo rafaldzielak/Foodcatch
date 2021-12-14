ddb7be0c9c95b15bee9639b41835bc53f68e1969
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-12-14 08:23:30 +0100

    FE: Apply coupon functionality & small refactor of order component

--- frontend/src/components/OrderComponent.tsx
+++ frontend/src/components/OrderComponent.tsx
@@ -1,6 +1,6 @@
 import "./order.scss";
 import "./address.scss";
-import React from "react";
+import React, { useState } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 import { AiFillMinusSquare, AiFillPlusSquare } from "react-icons/ai";
@@ -33,89 +33,117 @@ export const OrderComponent: React.FC<OrderComponentTypes> = ({
   const placeOrder = () => history.push("/order");
   const { dishes } = useTypedSelector((state) => (showOrder ? state.order : state.cart));
   const { updateCartAction } = useActions();
+  const [couponInput, setCouponInput] = useState("");
+  const [couponAppliedPercentage, setCouponAppliedPercentage] = useState(0);
 
-  const applyCouponHandler = () => {
+  const applyCouponHandler = (e: React.MouseEvent<HTMLFormElement, MouseEvent>) => {
+    e.preventDefault();
+    if (couponInput === "test20") setCouponAppliedPercentage(20);
     console.log("Apply coupon");
   };
+
+  const showPriceSummary = () => {
+    const priceWithoutCoupon =
+      dishes.reduce<number>((acc, dish) => (acc += dish.price * dish.quantity), 0) +
+      (showDelivery ? 9.99 : 0);
+    return (
+      <div className='final-price'>
+        <h3 className={`${couponAppliedPercentage && "grey"}`}>
+          Summary: {getNumberWithTwoDecimals(priceWithoutCoupon)} zł
+        </h3>
+        {couponAppliedPercentage !== 0 && (
+          <>
+            <h3>Coupon Applied: {couponAppliedPercentage}%</h3>
+            <h3>
+              Final Price:{" "}
+              {getNumberWithTwoDecimals((priceWithoutCoupon * (100 - couponAppliedPercentage)) / 100)} zł
+            </h3>
+          </>
+        )}
+      </div>
+    );
+  };
+  const showDishes = () =>
+    dishes.map((orderItem) => (
+      <React.Fragment key={orderItem.id + orderItem.name}>
+        <FadeIn>
+          <div className={`order-item ${size}`}>
+            <img src={orderItem.imgURL} alt='' />
+            <h4 className='title'>{orderItem.name}</h4>
+            {!hideAmountChooser && (
+              <div className='quantity'>
+                <span onClick={() => updateCartAction(orderItem.id, orderItem.quantity - 1)}>
+                  <AiFillMinusSquare />
+                </span>
+                <span className='number noselect'>{orderItem.quantity}</span>
+                <span onClick={() => updateCartAction(orderItem.id, orderItem.quantity + 1)}>
+                  <AiFillPlusSquare />
+                </span>
+              </div>
+            )}
+            <h3 className='price'>{getNumberWithTwoDecimals(orderItem.price * orderItem.quantity)} zł</h3>
+          </div>
+        </FadeIn>
+        <hr />
+      </React.Fragment>
+    ));
+
+  const showDeliveryIfApplicable = () =>
+    showDelivery && (
+      <FadeIn>
+        <div className={`order-item ${size}`}>
+          <h4 className='title'>Delivery</h4>
+          <h3 className='price'>9,99 zł</h3>
+          <div className='quantity'></div>
+        </div>
+        <hr />
+      </FadeIn>
+    );
+
+  const showSummaryArea = () => (
+    <>
+      <div className='summary'>
+        {showCouponInput && (
+          <div className='coupon'>
+            <form className='form-row' onClick={applyCouponHandler}>
+              <input
+                type='text'
+                placeholder='Coupon'
+                id='name'
+                value={couponInput}
+                onChange={(e) => setCouponInput(e.target.value)}></input>
+              <button>Apply Coupon</button>
+            </form>
+          </div>
+        )}
+        {showPriceSummary()}
+      </div>
+      {!hideButton && (
+        <button className='big' onClick={placeOrder}>
+          Continue
+        </button>
+      )}
+    </>
+  );
+
+  const showBackBtnIfApplicable = () =>
+    showBackBtn && (
+      <button className='small' onClick={() => history.push("/menu")}>
+        <MdKeyboardBackspace style={{ fontSize: "1.5rem" }} /> Back to Menu
+      </button>
+    );
 
   return (
     <div className={`order ${size}`}>
       <nav className='order-nav'>
-        {showBackBtn ? (
-          <button className='small' onClick={() => history.push("/menu")}>
-            <MdKeyboardBackspace style={{ fontSize: "1.5rem" }} /> Back to Menu
-          </button>
-        ) : (
-          <p />
-        )}
+        {showBackBtnIfApplicable()}
         <h2>Your Order</h2>
       </nav>
       <hr />
-      {dishes.map((orderItem) => (
-        <React.Fragment key={orderItem.id + orderItem.name}>
-          <FadeIn>
-            <div className={`order-item ${size}`}>
-              <img src={orderItem.imgURL} alt='' />
-              <h4 className='title'>{orderItem.name}</h4>
-              {!hideAmountChooser && (
-                <div className='quantity'>
-                  <span onClick={() => updateCartAction(orderItem.id, orderItem.quantity - 1)}>
-                    <AiFillMinusSquare />
-                  </span>
-                  <span className='number noselect'>{orderItem.quantity}</span>
-                  <span onClick={() => updateCartAction(orderItem.id, orderItem.quantity + 1)}>
-                    <AiFillPlusSquare />
-                  </span>
-                </div>
-              )}
-              <h3 className='price'>{getNumberWithTwoDecimals(orderItem.price * orderItem.quantity)} zł</h3>
-            </div>
-          </FadeIn>
-          <hr />
-        </React.Fragment>
-      ))}
-      {showDelivery && (
-        <FadeIn>
-          <div className={`order-item ${size}`}>
-            <h4 className='title'>Delivery</h4>
-            <h3 className='price'>9,99 zł</h3>
-            <div className='quantity'></div>
-          </div>
-          <hr />
-        </FadeIn>
-      )}
+      {showDishes()}
+      {showDeliveryIfApplicable()}
 
-      <div>
-        {dishes.length ? (
-          <>
-            <div className='summary'>
-              <div className='coupon'>
-                {showCouponInput && (
-                  <div className='form-row'>
-                    <input type='text' placeholder='Coupon' id='name'></input>
-                    <button onClick={applyCouponHandler}>Apply Coupon</button>
-                  </div>
-                )}
-              </div>
-              <h3>
-                Summary:{" "}
-                {getNumberWithTwoDecimals(
-                  dishes.reduce<number>((acc, dish) => (acc += dish.price * dish.quantity), 0) +
-                    (showDelivery ? 9.99 : 0)
-                )}{" "}
-                zł
-              </h3>
-            </div>
-            {!hideButton && (
-              <button className='big' onClick={placeOrder}>
-                Continue
-              </button>
-            )}
-          </>
-        ) : (
-          <h3>No items yet</h3>
-        )}
-      </div>
+      <div>{dishes.length ? showSummaryArea() : <h3>No items yet</h3>}</div>
     </div>
   );
 };
