1aad1dc1a281491492d690397d572914d1e5bf57
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-12-15 20:45:06 +0100

    FE: Alert fadeout seconds prop & success state

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Alert.scss";
 import { IoMdClose } from "react-icons/io";
 import FadeIn from "react-fade-in";
 
 interface PropTypes {
   hideCloseBtn?: boolean;
-  type?: "danger" | "warning" | "info";
+  type?: "danger" | "warning" | "info" | "success";
   closeModal?: () => {};
+  fadeOutSeconds?: number;
 }
 
-const Alert: React.FC<PropTypes> = ({ hideCloseBtn, type = "danger", children }) => {
+const Alert: React.FC<PropTypes> = ({ hideCloseBtn, type = "danger", children, fadeOutSeconds }) => {
+  const [returnEmpty, setReturnEmpty] = useState(false);
+
+  useEffect(() => {
+    if (fadeOutSeconds) {
+      const timer = setTimeout(() => {
+        setReturnEmpty(true);
+      }, fadeOutSeconds * 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [fadeOutSeconds]);
+
+  if (returnEmpty) return <> </>;
   return (
     <FadeIn>
       <div className={`alert alert-${type}`}>
