b455f49c4894bf358da0a138fdd38601f0ed70d3
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-01-17 18:09:35 +0100

    FE: useToggle fix

--- frontend/src/hooks/useToggle.ts
+++ frontend/src/hooks/useToggle.ts
@@ -3,13 +3,10 @@ import { useCallback, useState } from "react";
 function useToggle(initialValue?: boolean): readonly [boolean, (valueToSet?: boolean) => void] {
   const [value, setValue] = useState<boolean>(initialValue || false);
 
-  const toggle = useCallback(
-    () => (valueToSet?: boolean) => {
-      if (value !== undefined) setValue(valueToSet!);
-      else setValue((prev) => !prev);
-    },
-    [value]
-  );
+  const toggle = useCallback((valueToSet?: boolean) => {
+    if (valueToSet !== undefined) setValue(valueToSet!);
+    else setValue((prev) => !prev);
+  }, []);
 
   return [value, toggle] as const;
 }
