30279e548383d1ccdaccc14f52604872e22bdd09
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-02-04 08:40:04 +0100

    FE: All orders refactor

--- frontend/src/screens/AllOrders.tsx
+++ frontend/src/screens/AllOrders.tsx
@@ -22,61 +22,64 @@ const AllOrders = () => {
     refetch({ variables: { page: currentPage } });
   }, [currentPage, refetch]);
 
+  const renderOrdersTable = () => (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>Date</th>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Dishes</th>
+          <th>Payment</th>
+          <th>Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data?.getOrders?.orders.map((order) => {
+          const {
+            id,
+            email,
+            date,
+            firstName,
+            surname,
+            phone,
+            dishes,
+            paymentMethod,
+            city,
+            street,
+            streetNumber,
+          } = order;
+          return (
+            <tr key={order.id}>
+              <td>{id}</td>
+              <td>{email}</td>
+              <td>{format(convertStringDateToDate(date), "dd.MM.yyyy hh:mm")}</td>
+              <td>{`${firstName} ${surname}`}</td>
+              <td>{phone}</td>
+              <td>{dishes.length}</td>
+              <td>{paymentMethod[0].toUpperCase() + paymentMethod.substring(1)}</td>
+              <td>{`${street} ${streetNumber}, ${city}`}</td>
+              <td>
+                <Link to={`/summary/${id}`}>
+                  <button>Details</button>
+                </Link>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+
   if (loading) return <Loader />;
   return (
     <div className='container all-orders'>
       {error && <Alert hideCloseBtn>{error.message}</Alert>}
       {data && (
         <>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Email</th>
-                <th>Date</th>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Dishes</th>
-                <th>Payment</th>
-                <th>Address</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.getOrders?.orders.map((order) => {
-                const {
-                  id,
-                  email,
-                  date,
-                  firstName,
-                  surname,
-                  phone,
-                  dishes,
-                  paymentMethod,
-                  city,
-                  street,
-                  streetNumber,
-                } = order;
-                return (
-                  <tr key={order.id}>
-                    <td>{id}</td>
-                    <td>{email}</td>
-                    <td>{format(convertStringDateToDate(date), "dd.MM.yyyy hh:mm")}</td>
-                    <td>{`${firstName} ${surname}`}</td>
-                    <td>{phone}</td>
-                    <td>{dishes.length}</td>
-                    <td>{paymentMethod[0].toUpperCase() + paymentMethod.substring(1)}</td>
-                    <td>{`${street} ${streetNumber}, ${city}`}</td>
-                    <td>
-                      <Link to={`/summary/${id}`}>
-                        <button>Details</button>
-                      </Link>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-          {console.log(data?.getOrders)}
+          {renderOrdersTable()}
           <ReactPaginate
             className='react-paginate'
             breakLabel='...'
