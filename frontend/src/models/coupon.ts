4731b060b34509ab2efca3e587dd471eda4e9ee0
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-02-21 08:40:34 +0100

    FE: Coupon models

--- frontend/src/models/coupon.ts
+++ frontend/src/models/coupon.ts
@@ -2,3 +2,16 @@ export interface Coupon {
   couponApplied: string;
   couponAppliedPercentage: number;
 }
+
+export interface CouponFromDB {
+  couponName: string;
+  validUntil: string;
+  percentage: number;
+}
+
+export interface CouponResponse {
+  coupons: CouponFromDB[];
+  count: number;
+  page: number;
+  allPages: number;
+}
