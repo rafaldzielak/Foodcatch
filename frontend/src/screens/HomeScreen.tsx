e007680d7988a8b48500ef531b78816272cb6115
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-05-22 19:46:15 +0200

    FE: Add new photos & fix arrows on homescreen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -5,6 +5,12 @@ import TableBook from "./TableBook";
 import res from "../assets/img/res.jpg";
 import res2 from "../assets/img/res2.jpg";
 import res3 from "../assets/img/res3.jpg";
+import res4 from "../assets/img/res4.jpg";
+import res5 from "../assets/img/res5.jpg";
+import res6 from "../assets/img/res6.jpg";
+import res7 from "../assets/img/res7.jpg";
+import res8 from "../assets/img/res8.jpg";
+import res9 from "../assets/img/res9.jpg";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
@@ -27,6 +33,12 @@ const HomeScreen = () => {
         <img src={res} alt='' />
         <img src={res2} alt='' />
         <img src={res3} alt='' />
+        <img src={res4} alt='' />
+        <img src={res5} alt='' />
+        <img src={res6} alt='' />
+        <img src={res7} alt='' />
+        <img src={res8} alt='' />
+        <img src={res9} alt='' />
       </Carousel>
       <Link to='/menu'>
         <button className='big transparent mr-5'>Order Online</button>
